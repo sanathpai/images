aca7480b8b06ae40866eed6c6ded429226952a31
Sanath <sanathrpai99@gmail.com>
2020-01-09 21:49:06 +0530

    state getters actions mutations added

---
+++ src/store/modules/auth.js
@@ -0,0 +1,21 @@
+const state = {
+    token: null
+
+};
+const getters = {//used to query over state basically sort or compute
+    isLoggedIn: function (state)=> {
+    return !!state.token//!! =easy way to tearn a value into token
+}
+};
+const actions = {
+    logout: function ({ commit }) {//commit is used to call mutations
+        commit('setToken', null);
+    }
+
+};
+const mutations = {
+    setToken: function (state, token)=> {
+    state.token = token;
+
+}
+};
