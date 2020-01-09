057ede6b217dc042e3217773d50d56da2a0d0cc8
Sanath <sanathrpai99@gmail.com>
2020-01-09 21:03:58 +0530

    index created under store

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App';
+import store from './store';// no need to add index.Webpack will auto identify
 
 new Vue({
+    store: store,
     render: h => h(App)
 }).$mount('#app');
---
+++ src/store/index.js
@@ -0,0 +1,8 @@
+import Vuex from 'vuex';
+import Vue from 'vue';
+
+Vue.use(Vuex);//for communication between Vuex and Vue
+
+export default new Vuex.Store({
+    modules: {}
+});
