64231d745f90e8ebe7ebb0ca08660348363052ad
Sanath <sanathrpai99@gmail.com>
2020-01-12 10:56:03 +0530

    vscode shows syntax errors

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -1,26 +1,37 @@
 import api from '../../api/imgur';
+
+
 const state = {
     token: null
 
 };
-const getters = {//used to query over state basically sort or compute
+
+
+const getters = {
+    //used to query over state basically sort or compute
     isLoggedIn: function (state)=> {
-    return !!state.token//!! =easy way to tearn a value into token
+    return !!state.token
+    //!! =easy way to tearn a value into token
 }
 };
+
 const actions = {
     logout: function ({ commit }) {//commit is used to multi mutations easily
         commit('setToken', null);
-    }
-    login: function ()=>
-{
-    api.login();
-}
+    },
+
+    login: () => { api.login(); }
 
 };
 const mutations = {
-    setToken: function (state, token)=> {
+    setToken: function (state, token) => {
     state.token = token;
 
 }
 };
+export default {
+    state: state,
+    getters: getters,
+    actions: actions,
+    mutations: mutations
+}
