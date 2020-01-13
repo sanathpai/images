701429ea52c82dfc54e53392dafd91a0b9e65dba
Sanath <sanathrpai99@gmail.com>
2020-01-13 20:21:44 +0530

    learnt abou mapGetters

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="navbar navbar-dark bg-dark">
     <a href="/" class="navbar-brand">Image Storage</a>
+
     <ul class="navbar-nav ml-auto">
       <li class="nav item">
         <a class="nav-link" href="#" @click="login">Login</a>
@@ -11,10 +12,12 @@
 
 
 <script>
-import { mapActions } from "vuex"; //mapactions is a functions automatically connects diff actions
+import { mapActions, mapGetters } from "vuex"; //mapactions is a functions automatically connects diff actions
+
 export default {
   name: "AppHeader",
-  methods: mapActions(["login"])
+  methods: mapActions(["login"]),
+  computed: mapGetters(["isLoggedIn"])
 };
 </script>
 
