d43360a861178b7f4e4beb91d8e14ac5965c61b9
Sanath <sanathrpai99@gmail.com>
2020-01-13 19:57:50 +0530

    wired mapActions to AuthHandler

--- src/components/AuthHandler.vue
+++ src/components/AuthHandler.vue
@@ -4,10 +4,13 @@
 
 
 <script>
+import { mapActions } from "vuex";
 export default {
   name: "AuthHandler",
+  methods: mapActions(["finalizeLogin"]),
   created() {
     //refer docx for lifecycles under vue instance in official vue.js docx
+    this.finalizeLogin(window.location.hash); //window.location.has to get everything after # from url
   }
 };
 </script>
